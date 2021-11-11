70c9bd89fd4a698093e2fb00a758b44323e30f6e
mmarcotic <49272938+mmarcotic@users.noreply.github.com>
2021-11-11 10:29:09 +0100

    added functionality to test definitons

--- cypress/integration/log-in.spec.js
+++ cypress/integration/log-in.spec.js
@@ -12,31 +12,62 @@ describe('Log in site', () => {
     it('Navigate to log in screen', () => {
         // For this test to progress forward, it needs to get the correct site
         // the following will take us there
+        cy.contains('Log in').click()
+        cy.contains('EMAIL').click()
+        cy.get('[data-testid=EmaiSignup_Login]').click('left')
+        cy.contains('Forgot password').should('exist')
     })
 
     it('Try with invalid data', () => {
         // This test case will test that the user cannot log in with incorrect email and password
+        cy.fixture('user_information.json').then(text => {
+            cy.fillInField(text.invalid_email, 'EmaiSignup_Email')
+            cy.fillInField(text.invalid_password, 'EmaiSignup_Password')
+        })
+        cy.contains('LOG IN').click()
+        cy.contains('Invalid email address').should('exist')
 
     })
 
     it('Try valid only email', () => {
         // This test case will test that the user cannot log in with correct email and incorrect password
+        cy.fixture('user_information.json').then(text => {
+            cy.fillInField(text.valid_email, 'EmaiSignup_Email')
+            cy.fillInField(text.invalid_password, 'EmaiSignup_Password')
+        })
+        cy.contains('LOG IN').click()
+        cy.contains('Incorrect username or password', { timeout: 20000 }).should('exist')
 
     })
 
     it('Try valid only password', () => {
         // This test case will test that the user cannot log in with incorrect email and correct password
+        cy.fixture('user_information.json').then(text => {
+            cy.fillInField(text.invalid_email, 'EmaiSignup_Email')
+            cy.fillInField(text.valid_password, 'EmaiSignup_Password')
+        })
+        cy.contains('LOG IN').click()
+        cy.contains('Invalid email address').should('exist')
 
     })
 
     it('Try valid all', () => {
         // This test case will test that the user can log in
+        cy.fixture('user_information.json').then(text => {
+            cy.fillInField(text.valid_email, 'EmaiSignup_Email')
+            cy.fillInField(text.valid_password, 'EmaiSignup_Password')
+        })
+        cy.contains('LOG IN').click()
+        cy.contains('Log in', { timeout: 20000 }).should('not.exist')
 
     })
 
     it('Log out', () => {
         // This test case will test that the user can log out.
         // This primarily serves the purpose of setting browser up for other tests.
+        cy.logOut()
+        cy.contains('Profile').should('not.exist')
+        cy.wait(1000)
 
     })
 
